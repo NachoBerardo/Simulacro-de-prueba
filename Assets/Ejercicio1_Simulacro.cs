12725034604370816ff5a8e207d144064738a5c4
NachoBerardo <101335543+NachoBerardo@users.noreply.github.com>
2022-04-07 13:52:25 -0300

    Ej 1

---
+++ Assets/Ejercicio1_Simulacro.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio1_Simulacro : MonoBehaviour
+{
+
+    public int num1;
+    public int num2;
+
+    void Start()
+    {
+        if (num1 == num2)
+        {
+            Debug.Log("Son iguales");
+        }
+
+        else
+        {
+            Debug.Log("No son iguales");
+
+        }
+    }
+}
+
+
